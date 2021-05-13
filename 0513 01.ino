4e53f5da7f6c83ba80d8af4321951997f6feeac7
NotPerr <83740861+NotPerr@users.noreply.github.com>
2021-05-13 12:20:14 +0800

    Add files via upload

---
+++ 0513 01.ino
@@ -0,0 +1,43 @@
+byte seven_seg_digits[10][7]={{0,0,0,0,0,0,1},
+{1,0,0,1,1,1,1},
+{0,0,1,0,0,1,0},
+{0,0,0,0,1,1,0},
+{1,0,0,1,1,0,0},
+{0,1,0,0,1,0,0},
+{0,1,0,0,0,0,0},
+{0,0,0,1,1,1,1},
+{0,0,0,0,0,0,0},
+{0,0,0,1,1,0,0}
+};
+
+
+byte pin_numbers[7]={2,3,4,5,6,7,8};
+const int buttonPin=12;
+int buttonStste=0;
+int num=0;
+void setup() {
+    pinMode(2,OUTPUT);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+digitalWrite(9,0);
+digitalWrite(buttonPin,HIGH);
+pinMode(buttonPin,INPUT);
+}
+
+void sevenSegWrite(byte digit){
+  for(byte seg=0;seg<7;++seg){
+    digitalWrite(pin_numbers[seg],seven_seg_digits[digit][seg]);
+  }
+}
+void loop() {
+  num++;
+  if(num>9){
+    num=0;
+  }
+sevenSegWrite(num);
+delay(1000);
+}
---
+++ 0513_02.ino
@@ -0,0 +1,53 @@
+byte seven_seg_digits[10][7]={{0,0,0,0,0,0,1},
+{1,0,0,1,1,1,1},
+{0,0,1,0,0,1,0},
+{0,0,0,0,1,1,0},
+{1,0,0,1,1,0,0},
+{0,1,0,0,1,0,0},
+{0,1,0,0,0,0,0},
+{0,0,0,1,1,1,1},
+{0,0,0,0,0,0,0},
+{0,0,0,1,1,0,0}
+};
+
+
+byte pin_numbers[7]={2,3,4,5,6,7,8};
+const int buttonPin=12;
+int present_buttonState=0; int pre_buttonState=0;
+int buttonState=1;
+int num=0;
+void setup() {
+  Serial.begin(9600);
+    pinMode(2,OUTPUT);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+digitalWrite(9,0);
+digitalWrite(buttonPin,HIGH);
+pinMode(buttonPin,INPUT);
+}
+
+void sevenSegWrite(byte digit){
+  byte pin=2;
+  for(byte seg=0;seg<7;seg++){
+    digitalWrite(pin_numbers[seg],seven_seg_digits[digit][seg]);
+    pin++;
+  }
+}
+void loop() {
+  present_buttonState=digitalRead(buttonPin);
+  Serial.println(present_buttonState);
+  if(pre_buttonState==0 && present_buttonState==1){
+    num++;
+    if(num>9){
+      num=0;
+    }
+    pre_buttonState=1;
+  }
+
+  if(pre_buttonState==1 && present_buttonState==0) pre_buttonState=0;
+  sevenSegWrite(num);
+}
